77d8147c37778eb33a60d3318ceadf707087849c
juliuscaezarff <julius.caezar25@gmail.com>
2025-09-21 16:13:02 -0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
+import { z } from "zod";
 import { Elysia } from "elysia";
+import { openapi } from "@elysiajs/openapi";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(openapi())
+  .get("/", () => "Hello Elysia")
+  .get(
+    "/users/:id",
+    ({ params }) => {
+      const userId = params.id;
+
+      return { id: userId, name: "Julius Caezar" };
+    },
+    {
+      detail: {
+        summary: "Get user by ID",
+        description: "Retrieve a user by their unique ID",
+        tags: ["Users"],
+      },
+      params: z.object({
+        id: z.string(),
+      }),
+      response: {
+        200: z.object({
+          id: z.string(),
+          name: z.string(),
+        }),
+      },
+    }
+  )
+  .listen(3333);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
